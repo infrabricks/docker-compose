677322e47f991886cce39aecb2d170a35f9395e6
Peter Rossbach <peterrossbach@me.com>
2017-01-29 07:22:34 +0100

    Refactor: better build args and defaults. Add hub hooks/build
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- build-alpine.sh
+++ build-alpine.sh
@@ -3,11 +3,15 @@ IMAGE=docker-compose:alpine
 ACCOUNT=infrabricks
 TAG_LONG=1.10.0
 
-docker build -f Dockerfile.alpine \
- -t="${ACCOUNT}/$IMAGE" \
+docker build \
+ -f Dockerfile.alpine \
  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
  --build-arg VCS_REF=`git rev-parse --short HEAD` \
- .
+ --build-arg VERSION=$(git describe --tags) \
+ --build-arg VCS_URL=$(git config --get remote.origin.url) \
+ --build-arg VCS_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+ -t="${ACCOUNT}/$IMAGE" .
+
 DATE=`date +'%Y%m%d%H%M'`
 IID=$(docker inspect -f "{{.Id}}" ${ACCOUNT}/$IMAGE)
 docker tag ${IID} ${ACCOUNT}/$IMAGE-$DATE
